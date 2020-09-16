9ae166f31309df234bc494719c97c81ad92508e0
Adrian0910 <uchihasantander@gmail.com>
2020-09-16 11:37:47 -0500

    Variables Js

---
+++ ManejoVariables/public_html/js/programa.js
@@ -0,0 +1,24 @@
+document.write("<br/>Programa para declaracion de variables");
+
+//Declaracion de variables, en js se usa var
+var x = 5;
+var y = 2;
+var z = x + y;
+
+document.write("<br/>El valor de x es: " + x);
+
+document.write("<br/>El valor de y es: " + y);
+
+document.write("<br/>El valor de z es: " + z);
+
+
+document.writeln("<br/>");
+document.writeln("<br/>");
+
+var a = -5, b = 105, c = a+b;
+
+document.write("<br/>El valor de x es: " + a);
+
+document.write("<br/>El valor de y es: " + b);
+
+document.write("<br/>El valor de z es: " + c);
