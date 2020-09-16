8b489a58b24317a072238fffafd2bb89efb07de6
Adrian0910 <uchihasantander@gmail.com>
2020-09-16 11:36:19 -0500

    Html con Js

---
+++ ManejoHTML.JavaScript/public_html/js/programa.js
@@ -0,0 +1,7 @@
+function boton2(nombre){
+    alert("Hola " + nombre + " desde funcion2");
+}
+function mostrarNombre(parametro){
+    var nombre = parametro.value;
+    alert("Hola " + nombre + " desde mostrar nombre");
+}
