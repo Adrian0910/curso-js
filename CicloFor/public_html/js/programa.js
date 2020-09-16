64d8bfffb28dd62b2b6e8100aba1b208bc1c7f44
Adrian0910 <uchihasantander@gmail.com>
2020-09-16 11:28:29 -0500

    Ejemplo ciclo for Js

---
+++ CicloFor/public_html/js/programa.js
@@ -0,0 +1,18 @@
+
+
+function generarNumeros() {
+    var limite = 10;
+    for (i = 0; i < limite; i++) {
+
+document.getElementById("resultado").innerHTML += i + " ";
+    }
+    
+        
+    
+    }
+function limpiarIteracion() {
+    document.getElementById("resultado").innerHTML = "";
+}
+
+
+
