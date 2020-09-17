d8c90de5333ba08e2afaced57118e43d1397750b
Adrian0910 <uchihasantander@gmail.com>
2020-09-16 20:23:12 -0500

    Se modifico el proyecto de triangulo y se agrego frasesHTML

--- Triangulo/public_html/js/programa.js
+++
@@ -1,8 +0,0 @@
-/*function generarpiramide(base){
-    for (i=1; i<=base; i++){
-        for (j=0; j<i; j++){
-              document.write("*");
-        }
-        document.write("<br />");
-    }}
-*/
